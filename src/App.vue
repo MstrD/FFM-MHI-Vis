5ca7791521c808da17e1db0e86937723b6e2095c
MstrD <dadpmestre@gmail.com>
2020-03-30 13:41:48 +0100

    added loader

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    this.$q.loading.show({
+        message: 'Loading interface, please wait...'
+      })
+    // hiding in 3s
+    this.timer = setTimeout(() => {
+      this.$q.loading.hide()
+      this.timer = void 0
+    }, 2000)
+  },
+  beforeDestroy () {
+    if (this.timer !== void 0) {
+      clearTimeout(this.timer)
+      this.$q.loading.hide()
+    }
+  }
 }
 </script>
